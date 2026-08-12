d60881931ffe4120a2e0559fcc5d899388fd8017
subZero <marvinfordan08@gmail.com>
2026-08-12 22:10:30 +0800

    initial commit

---
+++ src/test/kotlin/com/sncfi/payment_system/PaymentSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sncfi.payment_system
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PaymentSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
